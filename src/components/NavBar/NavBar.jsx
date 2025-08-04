1c80ff74d5365497d5bf15e7c606c5896e21f839
SylviaRemington <sylviaremington@gmail.com>
2025-08-03 23:20:55 -0700

    adding links to home and nav page

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,12 +1,19 @@
+import { Link } from 'react-router';
+
 const NavBar = () => {
-  return (
-    <nav>
-      <ul>
-        <li>Home</li>
-        <li>Pokemon</li>
-      </ul>
-    </nav>
-  );
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to='/'>Home</Link>
+                </li>
+
+                <li>
+                    <Link to='/pokemon'>Pokemon</Link>
+                </li>
+            </ul>
+        </nav>
+    );
 };
 
 export default NavBar;
