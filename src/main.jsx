872796aa1f625336a1d1bdef42bf9ec68866da7d
SylviaRemington <sylviaremington@gmail.com>
2025-08-06 16:10:21 -0700

    wrap the browser router around the app

--- src/main.jsx
+++ src/main.jsx
@@ -6,6 +6,7 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    {/* Wrap the browser router around the app */}
     <BrowserRouter>
       <App />
     </BrowserRouter>
