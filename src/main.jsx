97963e67d72c97404765c7cbcce29e204736e507
SylviaRemington <sylviaremington@gmail.com>
2025-08-06 16:07:44 -0700

    adding addtl information about react router docs info

--- src/main.jsx
+++ src/main.jsx
@@ -11,3 +11,16 @@ createRoot(document.getElementById('root')).render(
     </BrowserRouter>
   </StrictMode>,
 )
+// If can't remember what to do for React Router, can go to 'React Router docs' 
+// and use the following:
+
+// Declarative
+// Declarative mode enables basic routing features like matching URLs to components, navigating around the app, and providing active states with APIs like <Link>, useNavigate, and useLocation.
+
+// import { BrowserRouter } from "react-router";
+
+// ReactDOM.createRoot(root).render(
+//   <BrowserRouter>
+//     <App />
+//   </BrowserRouter>,
+// );
