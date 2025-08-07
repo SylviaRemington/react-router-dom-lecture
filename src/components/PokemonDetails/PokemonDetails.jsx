35054963cbe0a6b199e4a9bc1496937197ce22d3
SylviaRemington <sylviaremington@gmail.com>
2025-08-06 21:38:33 -0700

    addtl PokemonDetails

--- src/components/PokemonDetails/PokemonDetails.jsx
+++ src/components/PokemonDetails/PokemonDetails.jsx
@@ -2,18 +2,23 @@ import { useParams } from 'react-router';
 
 const PokemonDetails = ({pokemon}) => {
     const { pokemonId } = useParams();
-    console.log({'all-pokemon': pokemon, pokemonId});
+    // console.log({'all-pokemon': pokemon, pokemonId});
+    // console.log(typeof pokemonId);
 
     const singlePokemon = pokemon.find((poke) => poke._id === Number (pokemonId));
-    console.log(singlePokemon);
+    // console.log(singlePokemon);
+
+    if (!id) {
+        return <p>No pokemon of that ID</p>;
+    }
 
   return (
     <>
-      <h2>Pokemon Details</h2>
+      <h2>Pokemon Details: {singlePokemon.name} </h2>
       <dl>
-        <dt>Weight:</dt>
+        <dt>Weight: {singlePokemon.weight}</dt>
         <dd></dd>
-        <dt>Height:</dt>
+        <dt>Height: {singlePokemon.height}</dt>
         <dd></dd>
       </dl>
     </>
