773ec0e64f95fcdf0edd3a2cbb88f6a7dbe085ab
SylviaRemington <sylviaremington@gmail.com>
2025-08-06 22:00:52 -0700

    adding New Pokemon link to navbar via NavBar.jsx

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -4,12 +4,19 @@ const NavBar = () => {
   return (
     <nav>
       <ul>
+
         <li>
           <Link to='/'>Home</Link>
         </li>
+
         <li>
           <Link to='/pokemon'>Pokemon</Link>
         </li>
+
+        <li>
+          <Link to='/pokemon/new'>New Pokemon</Link>
+        </li>
+
       </ul>
     </nav>
   );
